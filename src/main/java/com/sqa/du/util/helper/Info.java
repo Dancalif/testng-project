170e44635c220ff4ae915b10df39a41118ebf4da
Dancalif <dancalif@gmail.com>
2016-02-10 20:50:00 -0800

    Adding Info class

---
+++ src/main/java/com/sqa/du/util/helper/Info.java
@@ -0,0 +1,32 @@
+package com.sqa.du.util.helper;
+
+public class Info {
+
+	public static void display2DInfo(Object[][] objects) {
+
+	}
+
+	public static void displayInfo(Object obj) {
+
+	}
+
+	public static void displayInfo(Object obj1, Object obj2, Object... objN) {
+
+	}
+
+	public static void displayInfo(Object[] obj) {
+
+	}
+
+	public static String get2DInfo(Object[][] objects) {
+		return null;
+
+	}
+
+	public static String getInfo(Object obj) {
+		return null;
+		// StringBuilder sb = new Str
+
+	}
+
+}
